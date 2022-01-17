6f72630fc228aec4a06e13cee7d74b7c86701522
AlexandrP88 <alexsandr@rtural.ru>
2022-01-17 22:18:00 +0500

    modified test

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI Pustovalov [![Build status](https://ci.appveyor.com/api/projects/status/axph9px3ni8phos9/branch/master?svg=true)](https://ci.appveyor.com/project/AlexandrP88/pustovalovao45patterns/branch/master)
+
+
--- src/test/java/ru/netology/MeetingDateChange.java
+++ src/test/java/ru/netology/MeetingDateChange.java
@@ -5,6 +5,7 @@ import com.github.javafaker.Faker;
 import com.github.javafaker.Address;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -13,8 +14,7 @@ import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MeetingDateChange {
 
@@ -23,44 +23,44 @@ public class MeetingDateChange {
 
     @BeforeEach
     void setUpAll() {
-            faker =new Faker(new Locale("ru"));
+        faker = new Faker(new Locale("ru"));
     }
-        @Test
-        public void shouldSendForm () {
-            Configuration.holdBrowserOpen = true;
-            open("http://localhost:9999");
-            FormInfo info = DataGenerator.Form.generateInfo("ru");
-            $("[data-test-id='city'] input").val(info.getCity());
-            $("div[class='popup__content'] span").click();
-            String planningDate = generateDate(3);
-            $("[data-test-id='date'] input[class='input__control']").doubleClick()
-                    .val(planningDate).pressTab();
-            $("[data-test-id='name'] input").val(info.getName());
-            $(byName("phone")).setValue(info.getPhone());
-            $(byClassName("checkbox__box")).click();
-            $(withText("Запланировать")).click();
-            $("[data-test-id='success-notification']").shouldHave(text("Успешно!"), Duration.ofSeconds(15));
-            $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
-                    .shouldHave(exactText("Встреча успешно запланирована на " + planningDate));
-            String planningDate2 = generateDate(4);
-            $("[data-test-id='date'] input[class='input__control']").doubleClick()
-                    .val(planningDate2).pressTab();
-            $(withText("Запланировать")).click();
-            $("[data-test-id='replan-notification'] div[class='notification__title']").shouldBe(visible, Duration.ofSeconds(15))
-                    .shouldHave(text("Необходимо подтверждение"));
-            $(withText("Перепланировать")).shouldBe(visible).click();
-            $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
-                    .shouldHave(exactText("Встреча успешно запланирована на " + planningDate2));
 
-
-        }
-
-        public String generateDate ( int days){
-            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        }
-
+    @Test
+    public void shouldSendForm() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        FormInfo info = DataGenerator.Form.generateInfo("ru");
+        $("[data-test-id='city'] input").val(info.getCity());
+        $("div[class='popup__content'] span").click();
+        String planningDate = generateDate(4);
+        $("[data-test-id='date'] input[class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input[class='input__control']").setValue(planningDate).pressTab();
+        $("[data-test-id='name'] input").val(info.getName());
+        $(byName("phone")).setValue(info.getPhone());
+        $(byClassName("checkbox__box")).click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldHave(text("Успешно!"));//, Duration.ofSeconds(15));
+        $(".notification__content")//.shouldBe(visible)//, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + planningDate));
+        String planningDate2 = generateDate(5);
+        $("[data-test-id='date'] input[class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input[class='input__control']").setValue(planningDate2).pressTab();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] div[class='notification__title']").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Необходимо подтверждение"));
+        $(withText("Перепланировать")).shouldBe(visible).click();
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + planningDate2));
 
     }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
+}
 
 
 
